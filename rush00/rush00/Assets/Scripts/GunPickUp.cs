1f7856d97a3ddb794502df5996a9883baa3907ea
Allen <allenwem@gmail.com>
2019-03-16 14:22:29 -0700

    gun pick up

---
+++ rush00/rush00/Assets/Scripts/GunPickUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GunPickUP : MonoBehaviour
+{
+	public PlayerController _playerManager;
+	public Sprite GunImage;
+	public int	NumOfBullets;
+    // Start is called before the first frame update
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (Input.GetKeyDown("e") && _playerManager.HasGun == false)
+		{
+			_playerManager.HasGun = true;
+			_playerManager.Gun.GetComponent<SpriteRenderer>().sprite = GunImage;
+			_playerManager.Gun.SetActive(true);
+			Destroy(gameObject);
+		}
+	}
+}
--- rush00/rush00/Assets/Scripts/PlayerController.cs
+++ rush00/rush00/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 8f;
+	public bool	HasGun;
+	public GameObject Gun;
     public float direction;
     private float horizontal;
     private float vertical;
@@ -17,10 +19,11 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
+		Gun.SetActive(false);
+		HasGun = false;
         cc = GameObject.Find("Main Camera").GetComponent<Camera>();
         my = GetComponent<Transform>();
         body = GetComponent<Rigidbody2D>();
-
     }
     void Start()
     {
@@ -34,12 +37,21 @@ public class PlayerController : MonoBehaviour
         Vector3 mouse = cc.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
         float angle = Mathf.Atan2(mouse.y - my.position.y, mouse.x - my.position.x) * Mathf.Rad2Deg + 90;
         body.rotation = angle;
-
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
         //movement
         body.velocity = new Vector2(horizontal * speed, vertical * speed);
         //move cam
         cc.transform.position = new Vector3(my.position.x, my.position.y, -10);
+		DropGun();
     }
+	void DropGun()
+	{
+		if (Input.GetMouseButtonDown(1) && HasGun)
+		{
+			//add function drop gun forward;
+			HasGun = false;
+			Gun.SetActive(false);
+		}
+	}
 }
